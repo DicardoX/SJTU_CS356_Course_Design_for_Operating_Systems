8aaf581d4a22b5897174860b7c99f8b70b226c0c
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-06-06 21:50:13 +0800

    Create threadpool.h

---
+++ Project 5/Thread pool/threadpool.h
@@ -0,0 +1,8 @@
+#define MAX_TASK 7
+
+// function prototypes
+void execute(void (*somefunction)(void *p), void *p);
+int pool_submit(void (*somefunction)(void *p), void *p);
+void *worker(void *param);
+void pool_init(void);
+void pool_shutdown(void);
